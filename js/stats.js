32ff779f9839d7756b6060a14eba2a4c3fe89b58
Jim Narey <jimnarey@me.com>
2016-10-11 03:58:25 +0100

    Create stat container object

--- js/stats.js
+++ js/stats.js
@@ -29,39 +29,42 @@ class Stat {
 
 }
 
+//Refactor so that returned value is an object with lowercase names, e.g. damage_taken
+//Move all data, as far as possible, to a single JSON file
+
 function createStats(inkling) {
 
-    return [
+    return {
 
-        new Stat("Damage", "%", 0, 130, 100, function (x) {
+        damage: new Stat("Damage", "%", 0, 130, 100, function (x) {
             this.value = Math.min(this.max, 100 + 100 * x);
         }),
 
-        new Stat("Damage Taken", "%", 70, 100, 100, function (x) {
+        damage_taken: new Stat("Damage Taken", "%", 70, 100, 100, function (x) {
             this.value = Math.max(this.min, 100 * (1 - x / 1.8));
         }),
 
-        new Stat("Ink Recovery", "s", 1.65, 3, 200 / 3, function (x) {
+        ink_recovery: new Stat("Ink Recovery", "s", 1.65, 3, 200 / 3, function (x) {
             this.value = Math.max(this.min, 3 * (1.0 - x));
         }),
 
-        new Stat("Ink Usage Main", "%", 55, 100, (200 / 3), function (x) {
+        ink_usage_main: new Stat("Ink Usage Main", "%", 55, 100, (200 / 3), function (x) {
             this.value = Math.max(this.min, 100 - 100 * x);
         }),
 
-        new Stat("Ink Usage Sub", "%", 65, 100, 600 / 7, function (x) {
+        ink_usage_sub: new Stat("Ink Usage Sub", "%", 65, 100, 600 / 7, function (x) {
             this.value = Math.max(this.min, 100 - 100 * x);
         }),
 
-        new Stat("Bomb Throw Range", "%", 0, 150, 60, function (x) {
+        bomb_throw_range: new Stat("Bomb Throw Range", "%", 0, 150, 60, function (x) {
             this.value = Math.min(this.max, (156.8 * (1 + x)) / 156.8 * 100);
         }),
 
-        new Stat("Run Speed", "%", 0, 150, 60, function (x) {
+        run_speed: new Stat("Run Speed", "%", 0, 150, 60, function (x) {
             this.value = Math.min(this.max, (96 * (1 + x)) / 96 * 100);
         }),
 
-        new Stat("Swim Speed", "%", 100, 125, 120, function (x) {
+        swim_speed: new Stat("Swim Speed", "%", 100, 125, 120, function (x) {
             // var hasNinja = ($scope.mains.indexOf($scope.getAbilityByName("Ninja Squid")) != -1);
             // var hasPenalty = ($scope.selectedWeapon.speedPenalty);
 
@@ -73,15 +76,15 @@ function createStats(inkling) {
             this.value = Math.min(this.max, (192 * (1 + x)) / 192 * this.min);
         }),
 
-        new Stat("Special Charge", "%", 0, 130, 100, function (x) {
+        special_charge: new Stat("Special Charge", "%", 0, 130, 100, function (x) {
             this.value = Math.min(this.max, 100 + 100 * x);
         }),
 
-        new Stat("Special Time", "%", 0, 140, 75, function (x) {
+        special_time: new Stat("Special Time", "%", 0, 140, 75, function (x) {
             this.value = Math.min(this.max, 100 + 100 * x);
         }),
 
-        new Stat("Special Save", "%", 0, 100, 60, function (x) {
+        special_save: new Stat("Special Save", "%", 0, 100, 60, function (x) {
             if (inkling.weapon.depletion == 'Light') {
                 this.min = 60;
             } else if (inkling.weapon.depletion == 'Heavy') {
@@ -92,11 +95,11 @@ function createStats(inkling) {
             this.value = Math.min(this.max, this.min + 100 * x);
         }),
 
-        new Stat("Respawn Rate", "s", 2, (360 + 30 + 120) / 60, 45, function (x) {
+        respawn_rate: new Stat("Respawn Rate", "s", 2, (360 + 30 + 120) / 60, 45, function (x) {
             this.value = Math.max(this.min, ((1.0 - x) * 360 + 30 + 120) / 60);
         }),
 
-        new Stat("Jump Speed", "s", 2.41, 4.83, 60, function (x) {
+        jump_speed: new Stat("Jump Speed", "s", 2.41, 4.83, 60, function (x) {
             // var hasStealth = ($scope.mains.indexOf($scope.getAbilityByName("Stealth Jump")) != -1);
             var a = 20 + 60 * (1 - x);
             var b = 130 * (1 - x);
@@ -105,10 +108,10 @@ function createStats(inkling) {
             this.value = Math.max(this.min, (a + b + c) / 60);
         }),
 
-        new Stat("Echolocator/Haunt Duration", "s", 4.5, 9, 1, function (x) {
+        echo_haunt_time: new Stat("Echolocator/Haunt Duration", "s", 4.5, 9, 1, function (x) {
             this.value = x > 0 ? 4.5 : 9;
         })
-    ];
+    };
 
 }
 
