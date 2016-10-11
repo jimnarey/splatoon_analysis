56c96f4af3ec798c9161922a39180cef74c9f7fa
Jim Narey <jimnarey@me.com>
2016-10-11 16:24:35 +0100

    Weapons and abilities now use json

---
+++ js/data.js
@@ -0,0 +1,20 @@
+/**
+ * Created by jimnarey on 11/10/16.
+ */
+let fs = require('fs');
+
+let path = '../json/';
+
+var weapons = JSON.parse(fs.readFileSync(path + 'weapons.json', 'utf8'));
+var abilities = JSON.parse(fs.readFileSync(path + 'abilities.json', 'utf8'));
+var subweapons = JSON.parse(fs.readFileSync(path + 'subweapons.json', 'utf8'));
+var specials = JSON.parse(fs.readFileSync(path + 'specials.json', 'utf8'));
+var brands = JSON.parse(fs.readFileSync(path + 'brands.json', 'utf8'));
+var gear = JSON.parse(fs.readFileSync(path + 'gear.json', 'utf8'));
+
+exports.weapons = weapons;
+exports.abilities = abilities;
+exports.subweapons = subweapons;
+exports.specials = specials;
+exports.brands = brands;
+exports.gear = gear;
